11f1dc20f204e41160b9cfd368dfbe9b1f6831ee
mosfeqanik <mosfeqanik01@gmail.com>
2021-07-09 14:19:30 +0600

    api called and customized done

--- lib/controller/CurrencyController.dart
+++ lib/controller/CurrencyController.dart
@@ -5,7 +5,9 @@ import 'package:currency_converter/models/Currency.dart';
 
 class CurrencyController {
   Future<Currency> getCurrency(String formCurrency) async {
+    //receiving response from network
     String response = await network.loadCurrencyData(formCurrency);
+    //adding response to modal
     Currency currency = Currency.fromJson(jsonDecode(response));
     return currency;
   }
